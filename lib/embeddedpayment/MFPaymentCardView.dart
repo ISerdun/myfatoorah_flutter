16faa1d0911c1de14d498cc92255b0f0ae7b93d6
Dmitrii Serdun <serdun.dmitrii@steelkiwi.com>
2021-12-17 15:36:40 +0200

    Fixed bug with focus

--- lib/embeddedpayment/MFPaymentCardView.dart
+++ lib/embeddedpayment/MFPaymentCardView.dart
@@ -114,7 +114,7 @@ class MFPaymentCardView extends StatefulWidget {
       <head>
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
           <title>Payment Page</title>
           <style></style>
       </head>
